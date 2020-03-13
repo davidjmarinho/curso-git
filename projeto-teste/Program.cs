7f293172195f4eee47e381bf35f3a9ffce6f3c51
David Pereira <davidjmarinhopereira@gmail.com>
2020-03-13 17:46:20 -0300

    Adicionados tracos

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -6,10 +6,12 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("----------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hi Bits");
             Console.WriteLine("Have a nice day!");
             Console.ReadLine();
+            Console.WriteLine("----------------------------");
         }
     }
 }
